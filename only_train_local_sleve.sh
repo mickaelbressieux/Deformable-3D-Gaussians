9db3cc1b7cc4dcb97eb9f5a0789aa2f873b3374a
Mickael Bressieux <mbressieux@ethz.ch>
2024-08-15 20:22:10 +0200

    cleanup

--- only_train_local_sleve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=/M2SSD1/Datasets/NeRF-DS.dataset/sieve_novel_view
-LOG=/M2SSD1/Logs/Def_3DGS/sieve_novel_view
-
-python train.py -s ${DATA_DIR} -m ${LOG} --eval --iterations 20000
--- render_chickchicken_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LOG=/M2SSD1/Logs/Def_3DGS/chickchicken
-
-python render.py -m ${LOG} --mode segment --skip_test
